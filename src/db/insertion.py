004620a46dffcb67f8e4c917ba319619a452e62a
BVasquez07 <bvasq07@gmail.com>
2025-12-01 19:55:24 -0500

    feat: wrote yfinance extraction code and insertion code for transformed data into stock table

--- src/db/insertion.py
+++ src/db/insertion.py
@@ -6,6 +6,8 @@ TODO:
  -> might not be
 - error handling
     -> timeout handling
+    -> ticker error handling
+    -> implement simulaton data insertion when simulation is added to the Transform stage!
 
 Processing is as follows:
 
@@ -17,34 +19,20 @@ Processing is as follows:
 """
 
 
-def insert_data(db_host_addr: str, db_port: str, db_name: str, db_user: str, db_password: str, db_timeout: int, data: list[]) -> None: #cool thing to look into is how to make use of the *args and **kwargs in python functions
-
-
-    try:
-        psycopg.connect(f"hostaddr={db_host_addr} port={db_port} dbname={db_name} user={db_user} password={db_password} connect_timeout={db_timeout}").close()
-    except psycopg.DatabaseError:
-        #creates the database if it does not exist by connecting to the default 'postgres' database
-        with psycopg.connect(f"hostaddr={db_host_addr} port={db_port} dbname=postgres user={db_user} password={db_password} connect_timeout={db_timeout}") as conn:
-            conn.autocommit = True #this helps as it automatically commits each statement without needing to call conn.commit()!!!
-            with conn.cursor() as cur:
-                cur.execute(f"CREATE DATABASE {db_name};")
-    finally:
-        print(f'Connected to database {db_name} successfully!')
-
-    #for refrence on the param kwargs: https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-PARAMKEYWORDS
+def insert_data(db_host_addr: str, db_port: str, db_name: str, db_user: str, db_password: str, db_timeout: int, data: list[dict[str]]) -> None:
     with psycopg.connect(f"hostaddr={db_host_addr} port={db_port} dbname={db_name} user={db_user} password={db_password} connect_timeout={db_timeout}") as conn:
         with conn.cursor() as cur:
-            
-            #lets put insertion query here then we can print it with the code below
-            #here we might have to do a loop over the data for mass insertions
-
-
-
-            #this will print out the data to make sure we actually inserted the proper data
+            cur.executemany("""
+                INSERT INTO stock_data (ticker, date, open, high, low, close, adj_close, volume)
+                VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+            """, data) #data needs to be a list of tuples [('ticker', 'date', 'open', 'high', 'low', 'close', 'adj_close', 'volume'), (...), ...]
+            #this will print out the data to make sure we actually inserted the proper data AND IS TEMPORARY FOR TESTING PURPOSES
             cur.execute("SELECT * from stock_data;")
             print(cur.fetchall()) # can use this method or iterate over the cursor...
-
             cur.execute("SELECT * from simulation;")
             print(cur.fetchall())
 
             conn.commit()
+
+
+            
